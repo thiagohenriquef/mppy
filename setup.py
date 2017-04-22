5f551eba3612c11c8273f40a7ccb12820fa4f9db
Thiago <thiago.ferreira10@hotmail.com>
2017-04-21 22:18:52 -0300

    new release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author = 'Thiago Henrique Ferreira',
     author_email = 'thiagohferreira10@gmail.com',
     url = 'https://github.com/thiagohenriquef/mppy',
-    download_url = 'https://github.com/thiagohenriquef/mppy/archive/0.3.tar.gz',
+    download_url = 'https://github.com/thiagohenriquef/mppy/archive/0.3.4.tar.gz',
     keywords = ['multidimensional projection', 'data visualization', 'dimensionality reduction'],
     classifiers=['Development Status :: 4 - Beta','Programming Language :: Python :: 3.5', 'Programming Language :: Python :: 3.6'],
     install_requires = ['numpy>=1.11.0', 'scipy>=0.17.0', 'matplotlib>=1.5.1', 'scikit-learn>=0.17'],
