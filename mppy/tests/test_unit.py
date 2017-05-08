08fe9e4d2864ea1576b14bcec7bbd74f7da5d781
Thiago <thiago.ferreira10@hotmail.com>
2017-05-08 00:38:56 -0300

    improve time of force scheme technique

--- mppy/tests/test_unit.py
+++ mppy/tests/test_unit.py
@@ -1,12 +1,18 @@
+class MyTest(unittest.TestCase):
+
+    def test(self):
+        import numpy as np
+        import mppy
+        import os
+
+        print(path.abspath(path.join(__file__, "../..")), "/datasets/iris.data")
+        data = np.loadtxt(path.abspath(path.join(__file__, "../..")), "/datasets/iris.data", delimiter=",")
+        result = mppy.force_2d(data)
+
+        print("Stress: ", mppy.normalized_kruskal_stress(data, result))
+
+        mppy.simple_scatter_plot(result, data[:, data.shape[1] - 1])
+
+
 if __name__ == "__main__":
-    import numpy as np
-    import mppy
-    import os
-
-    print(path.abspath(path.join(__file__ ,"../..")),"/datasets/iris.data")
-    data = np.loadtxt(path.abspath(path.join(__file__ ,"../..")),"/datasets/iris.data", delimiter=",")
-    result = mppy.force_2d(data)
-
-    print("Stress: ",mppy.normalized_kruskal_stress(data, result))
-
-    mppy.simple_scatter_plot(result, data[:, data.shape[1]-1])
+    unittest.main()
