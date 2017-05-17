da782f94f45fe14288d8105ff3eda63a7aa6e275
Thiago <thiago.ferreira10@hotmail.com>
2017-05-17 02:14:51 -0300

    new interactive scatter plot

--- mppy/__init__.py
+++ mppy/__init__.py
@@ -7,7 +7,7 @@ from mppy.lamp import lamp_2d
 from mppy.lsp import lsp_2d
 from mppy.pekalska import pekalska_2d
 from mppy.plmp import plmp_2d
-from mppy.plot import interactive_scatter_plot, simple_scatter_plot, delaunay_scatter
+from mppy.plot import interactive_scatter_plot, simple_scatter_plot, delaunay_scatter, interactive_scatter_plot2
 from mppy.sammon import sammon
 from mppy.stress import kruskal_stress, normalized_kruskal_stress
 from mppy.neighborhood import neighborhood_preservation, neighborhood_hit
--- tests/test_techniques.py
+++ tests/test_techniques.py
@@ -4,17 +4,18 @@ if __name__ == "__main__":
     from os.path import dirname, abspath
     import os
 
-    #data = np.loadtxt(dirname(os.getcwd())+"/datasets/iris.data", delimiter=",")
-    data = np.loadtxt("/home/thiago/TCC/Java códigos tácito/datasets/diabetes-python.data", delimiter=";")
+    data = np.loadtxt(dirname(os.getcwd())+"/datasets/wdbc.data", delimiter=",")
+    #data = np.loadtxt("/home/thiago/TCC/Java códigos tácito/datasets/diabetes-python.data", delimiter=";")
     clusters = data[:, data.shape[1] - 1]
     #print("Carregado conjunto de dados")
     result = mppy.force_2d(data[:, 1:data.shape[1] - 1])
 
-    print(mppy.neighborhood_preservation(data[:, 1:data.shape[1]-1], result, clusters, 15))
-    print(mppy.neighborhood_hit(result, clusters, 15))
+    #print(mppy.neighborhood_preservation(data[:, 1:data.shape[1]-1], result, clusters, 15))
+    #print(mppy.neighborhood_hit(result, clusters, 15))
     mppy.normalized_kruskal_stress(data[:, 1:data.shape[1] - 1], result)
     # print(mppy.kruskal_stress(data,result))
 
-    mppy.simple_scatter_plot(result, data[:, data.shape[1] - 1])
+    #mppy.simple_scatter_plot(result, data[:, data.shape[1] - 1])
     #mppy.delaunay_scatter(result, data, data[:, data.shape[1] - 1])
     #mppy.interactive_scatter_plot(result, data, data[:, data.shape[1] - 1])
+    mppy.interactive_scatter_plot2(result, data, data[:, data.shape[1] - 1])
